6858b12781ae21d619db16cc8dcc1eff0309a769
Crocodyl <unknownsushant.0042@gmail.com>
2026-01-05 18:23:42 +0530

    character hashing

---
+++ allPatterns/src/characterHashing.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class characterHashing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s= sc.nextLine();
+        System.out.println("character u wnat to find frequency :");
+        char c = sc.next().charAt(0);
+
+        System.out.println(frequency(c,s));
+
+
+
+    }
+    public static int frequency(char c, String s){
+        int count =0;
+        int len = s.length();
+        for(int i =0; i<len; i++){
+            if(s.charAt(i) == c) {
+
+                count++;
+            }
+        }
+        return count;
+    }
+}
