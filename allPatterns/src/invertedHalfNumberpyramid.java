65f2324705e0dff22a357acf3905039e30c57695
Crocodyl <unknownsushant.0042@gmail.com>
2026-01-06 22:48:51 +0530

    invertedHalfPyramid

---
+++ allPatterns/src/invertedHalfNumberpyramid.java
@@ -0,0 +1,22 @@
+public class invertedHalfNumberpyramid {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0; i<n; i++){
+            for(int j =1; j<=n-i ;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*Output
+
+12345
+1234
+123
+12
+1
+
+ */
