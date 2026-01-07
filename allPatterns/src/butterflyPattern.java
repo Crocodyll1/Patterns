eaf1d844f7246ec42feb411422130322e228c3df
Crocodyl <unknownsushant.0042@gmail.com>
2026-01-07 18:24:08 +0530

    butterflyPattern

---
+++ allPatterns/src/butterflyPattern.java
@@ -0,0 +1,59 @@
+public class butterflyPattern {
+    public static void main(String[] args) {
+        int n =4;
+        for(int i =0; i<n; i++){
+            for(int j =0; j<i+1; j++){
+                System.out.print(" * ");
+            }
+            for(int k =0; k<n-i-1; k++){
+                System.out.print("   ");
+
+            }
+            for(int j =0; j<=n-i-2; j++){
+                System.out.print("   ");
+            }
+            for(int j= 0; j<=i; j++){
+                System.out.print(" * ");
+            }
+
+
+            System.out.println();
+        }
+        for(int i =0; i<n; i++){
+            for(int j =0; j<n-i; j++){
+                System.out.print(" * ");
+            }
+            for(int k =0; k<i; k++){
+                System.out.print("   ");
+
+            }
+
+            for(int j =0; j<i; j++){
+                System.out.print("   ");
+            }
+            for(int j= 0; j< n-i; j++){
+                System.out.print(" * ");
+            }
+
+
+            System.out.println();
+        }
+    }
+}
+
+/*Output
+ *                    *
+ *  *              *  *
+ *  *  *        *  *  *
+ *  *  *  *  *  *  *  *
+ *  *  *  *  *  *  *  *
+ *  *  *        *  *  *
+ *  *              *  *
+ *                    *
+
+
+ */
+
+/*
+you can print the spaces in one loop, it also works upperhalf and lowerhalf.
+ */
