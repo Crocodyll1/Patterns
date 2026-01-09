3763289945f31f581993adfbda5d96d2c7237cf5
Sushant Yadav <unknownsushant.0042@gmail.com>
2026-01-09 23:57:00 +0530

    Number Spaced pyramid

---
+++ allPatterns/src/SameNumberPyramid.java
@@ -0,0 +1,16 @@
+public class SameNumberPyramid {
+    public static void main(String[] args) {
+        int n =5;
+
+        for(int i =1; i<=5; i++){
+            for(int j =n-i; j>=1; j--){
+                System.out.print(" ");
+            }
+            for(int k =1; k<=i;k++){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
